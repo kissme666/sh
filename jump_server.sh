0acd085145e9567108c79d48d59405682da423cf
root <root@localhost.localdomain>
2018-12-19 12:19:45 +0800

    可退出美化版

--- jump_server.sh
+++ jump_server.sh
@@ -1,13 +1,19 @@
 #!/usr/bin/env bash 
 centos=192.168.199.157
 ubuntu_server=192.168.199.77
-ubuntu_desktop=192.168.199
+ubuntu_desktop=192.168.199.x
+clear
 while : 
 do
-	echo "Jump Server Manger"
-	echo "1. centos 1"
-	echo "2. ubuntu server 18.04"
-	echo "3. ubuntu desktop 18.04"
+	cat<<-EOF
+	+----------------------------+
+	|    Jump Server Manger	     |
+	|    1. centos 1	     |
+	|    2. ubuntu server 18.04  |
+	|    3. ubuntu desktop 18.04 |
+	|    q. exit 	             |
+	+----------------------------+
+	EOF
 	echo -n "input you choice : "
 	read num
 	case $num in
@@ -17,6 +23,8 @@ do
 		;;
 		3) ssh air@$ubuntu_desktop
 		;;
+		q) exit 0
+		u;; 
 		*) echo "请重新选择"
 		;;
 	esac
